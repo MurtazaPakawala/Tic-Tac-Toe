1ba4acc7e04433ee04abe3ecf1a9d9db54296f14
Murtaza Pakawala <mypaka52@gmail.com>
2022-03-12 22:20:47 +1100

    completed with the project : )

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
     }
 
     private fun initializeBoard() {
+        TURN=0;
             for(i in 0..2)
             {
                 for(j in 0..2)
@@ -84,6 +85,75 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
                 }
 
             }
+        checkWinner()
+    }
+
+    private fun checkWinner() {
+        //check row
+        for(i in 0..2)
+        {
+            if(BoardStatus[i][0]==BoardStatus[i][1]&&BoardStatus[i][0]==BoardStatus[i][2]&&BoardStatus[i][0]!=-1)
+            {
+                if(BoardStatus[i][0]==1)
+                {
+                    tvDisplay.text="X is the Winner!!"
+                    disableBtn()
+                }else{
+                    tvDisplay.text="0 is the Winner!!"
+                    disableBtn()
+                }
+            }
+        }
+        // check for column
+        for(i in 0..2)
+        {
+            if(BoardStatus[0][i]==BoardStatus[1][i]&&BoardStatus[0][i]==BoardStatus[2][i]&&BoardStatus[0][i]!=-1)
+            {
+                if(BoardStatus[0][i]==1)
+                {
+                    tvDisplay.text="X is the Winner!!"
+                    disableBtn()
+                }else{
+                    tvDisplay.text="0 is the Winner!!"
+                    disableBtn()
+                }
+            }
+        }
+        //checking for the diagnol
+        // first i=j diagnol
+        if(BoardStatus[0][0]==BoardStatus[1][1]&&BoardStatus[0][0]==BoardStatus[2][2]&&BoardStatus[0][0]!=-1)
+        {
+            if(BoardStatus[0][0]==1)
+            {
+                tvDisplay.text="X is the Winner!!"
+                disableBtn()
+            }else{
+                tvDisplay.text="0 is the Winner!!"
+                disableBtn()
+            }
+        }
+        // checking second diagnol
+        if(BoardStatus[0][2]==BoardStatus[1][1]&&BoardStatus[0][2]==BoardStatus[2][0]&&BoardStatus[0][2]!=-1)
+        {
+            if(BoardStatus[0][2]==1)
+            {
+                tvDisplay.text="X is the Winner!!"
+                disableBtn()
+            }else{
+                tvDisplay.text="0 is the Winner!!"
+                disableBtn()
+            }
+        }
+    }
+
+    private fun disableBtn() {
+        for(i in 0..2)
+        {
+            for(j in 0..2)
+            {
+                board[i][j].isEnabled=false;
+            }
+        }
     }
 
     private fun updateBoard(row: Int, col: Int, player: Boolean) {
@@ -92,12 +162,15 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         var score=if(player) 1 else 0
         BoardStatus[row][col]=score
         board[row][col].text=text
+        board[row][col].isEnabled=false
         PLAYER=!PLAYER
         text= if(text=="X") "0" else "X"
         tvDisplay.text="the next turn is of ${text}"
         if(TURN==9)
         {
-            initializeBoard()
+            tvDisplay.text="Match is DRAW!!!"
+            disableBtn()
         }
     }
+
 }
